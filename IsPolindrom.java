b3bcd82de7045265abd73fd92ee7106759176ce5
SaddamHasanov <123798376+SaddamHasanov@users.noreply.github.com>
2023-05-09 22:22:36 +0400

    Update IsPolindrom.java

--- IsPolindrom.java
+++ IsPolindrom.java
@@ -1,5 +1,5 @@
 // Polindrom olub olmadigini yoxlayir
-	public void IsPolindrom() {
+	public void IsPolindrom(int [] array) {
 	boolean isPolindrom = true;
 	for(int i = 0; i < array.length/2; i++) {
 			if(array[i] != array[array.length - 1 - i]) {
