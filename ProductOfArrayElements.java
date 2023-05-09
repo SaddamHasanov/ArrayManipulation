86acaf671b792e6a01884fa5541ca5980453e588
SaddamHasanov <123798376+SaddamHasanov@users.noreply.github.com>
2023-05-09 21:41:39 +0400

    Create ProductOfArrayElements.java

---
+++ ProductOfArrayElements.java
@@ -0,0 +1,8 @@
+// Elementlerin Hasili
+	public void Product() {
+		long product = 1;
+		for(int i = 0; i < array.length; i++) {
+			product *= array[i];
+		}
+		System.out.println("Product of array elements : " + product);
+	}
